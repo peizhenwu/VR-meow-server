e436aaffce3ff85e4b7dae6945ed30118b4a4af1
peizhenwu <32442789+peizhenwu@users.noreply.github.com>
2020-12-07 16:31:43 -0800

    setup server

---
+++ index.js
@@ -0,0 +1,42 @@
+const express = require('express');
+const cors = require('cors');
+const fs = require('fs');
+
+const interval = 2000;
+
+const app = express();
+app.use(cors());
+
+app.get('/', (req, res) => {
+    let rawdata = fs.readFileSync('room.json');
+    let room = JSON.parse(rawdata);
+    res.json(room);
+});
+
+app.get('/update', (req, res) => {
+    let name = req.params.name;
+    let rawdata = fs.readFileSync('room.json');
+    let room = JSON.parse(rawdata);
+    res.json(room);
+});
+
+app.get('/feedSpecial', (req, res) => {
+    let rawdata = fs.readFileSync('room.json');
+    let room = JSON.parse(rawdata);
+    room.state = "feedSpecial";
+    let data = JSON.stringify(room);
+    fs.writeFileSync('room.json', data);
+
+    setTimeout(()=>{
+        let rawdata2 = fs.readFileSync('room.json');
+        let room2 = JSON.parse(rawdata2);
+        room2.state = "none";
+        let data2 = JSON.stringify(room2);
+        fs.writeFileSync('room.json', data2);
+    },interval);
+    res.json();
+});
+
+app.listen(2020, () => {
+    console.log('server is listening on port 2020');
+});
