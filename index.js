befd43c887a453ea885aa1c764ff589370fd7593
peizhenwu <32442789+peizhenwu@users.noreply.github.com>
2020-12-10 20:35:58 -0800

    add server functions

--- index.js
+++ index.js
@@ -7,6 +7,7 @@ const interval = 2000;
 const app = express();
 app.use(cors());
 
+
 app.get('/', (req, res) => {
     let rawdata = fs.readFileSync('room.json');
     let room = JSON.parse(rawdata);
@@ -24,6 +25,7 @@ app.get('/feedSpecial', (req, res) => {
     let rawdata = fs.readFileSync('room.json');
     let room = JSON.parse(rawdata);
     room.state = "feedSpecial";
+    room.feedSpecialCount += 1;
     let data = JSON.stringify(room);
     fs.writeFileSync('room.json', data);
 
@@ -37,6 +39,111 @@ app.get('/feedSpecial', (req, res) => {
     res.json();
 });
 
+app.get('/feedWet', (req, res) => {
+    let rawdata = fs.readFileSync('room.json');
+    let room = JSON.parse(rawdata);
+    switch(req.query.index){
+        case "1":
+            room.indivState1 = "feedWet";
+            break;
+        case "2":
+            room.indivState2 = "feedWet";
+            break;
+        case "3":
+            room.indivState3 = "feedWet";
+            break;
+    }
+    room.cans -= 1;
+    let data = JSON.stringify(room);
+    fs.writeFileSync('room.json', data);
+
+    setTimeout(()=>{
+        let rawdata2 = fs.readFileSync('room.json');
+        let room2 = JSON.parse(rawdata2);
+        switch(req.query.index){
+            case "1":
+                room2.indivState1 = "none";
+                break;
+            case "2":
+                room2.indivState2 = "none";
+                break;
+            case "3":
+                room2.indivState3 = "none";
+                break;
+        }
+        let data2 = JSON.stringify(room2);
+        fs.writeFileSync('room.json', data2);
+        console.log("finish feeding wet");
+    },interval);
+    res.json();
+});
+
+app.get('/displayTree', (req, res) => {
+    let rawdata = fs.readFileSync('room.json');
+    let room = JSON.parse(rawdata);
+    room.displayTree = true;
+    let data = JSON.stringify(room);
+    fs.writeFileSync('room.json', data);
+    res.json();
+});
+
+app.get('/updateTree', (req, res) => {
+    let rawdata = fs.readFileSync('room.json');
+    let room = JSON.parse(rawdata);
+    room.treePosX = parseFloat(req.query.x);
+    room.treePosZ = parseFloat(req.query.z);
+    let data = JSON.stringify(room);
+    fs.writeFileSync('room.json', data);
+    res.json();
+});
+
+app.get('/displayBoard', (req, res) => {
+    let rawdata = fs.readFileSync('room.json');
+    let room = JSON.parse(rawdata);
+    room.displayBoard = !room.displayBoard;
+    let data = JSON.stringify(room);
+    fs.writeFileSync('room.json', data);
+    res.json();
+});
+
+app.get('/updateBoard', (req, res) => {
+    let rawdata = fs.readFileSync('room.json');
+    let room = JSON.parse(rawdata);
+    room.boardPosX = parseFloat(req.query.x);
+    room.boardPosZ = parseFloat(req.query.z);
+    let data = JSON.stringify(room);
+    fs.writeFileSync('room.json', data);
+    res.json();
+});
+
+app.get('/displayElephant', (req, res) => {
+    let rawdata = fs.readFileSync('room.json');
+    let room = JSON.parse(rawdata);
+    room.displayElephant = !room.displayElephant;
+    let data = JSON.stringify(room);
+    fs.writeFileSync('room.json', data);
+    res.json();
+});
+
+app.get('/updateElephant', (req, res) => {
+    let rawdata = fs.readFileSync('room.json');
+    let room = JSON.parse(rawdata);
+    room.elephantPosX = parseFloat(req.query.x);
+    room.elephantPosZ = parseFloat(req.query.z);
+    let data = JSON.stringify(room);
+    fs.writeFileSync('room.json', data);
+    res.json();
+});
+
+app.get('/updateCans', (req, res) => {
+    let rawdata = fs.readFileSync('room.json');
+    let room = JSON.parse(rawdata);
+    room.cans = parseFloat(req.query.cans);
+    let data = JSON.stringify(room);
+    fs.writeFileSync('room.json', data);
+    res.json();
+});
+
+
 app.listen(process.env.PORT, () => {
-    console.log('server is listening on port 2020');
 });
